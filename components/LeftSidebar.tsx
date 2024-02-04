11d5f37680aec9b5d38ec0bcfcf994ab668f6f23
김우빈 <kwb020312@naver.com>
2024-02-04 14:21:39 +0900

    Comment, PinnedComposer

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,6 +1,8 @@
 const LeftSidebar = () => {
   return (
-    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20"></section>
+    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
+      <h3 className="px-5 pt-4 text-xs">ELEMENTS</h3>
+    </section>
   );
 };
 
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,5 +1,9 @@
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
+      <h3 className="px-5 pt-4 text-xs">DESIGN</h3>
+    </section>
+  );
 };
 
 export default RightSidebar;
