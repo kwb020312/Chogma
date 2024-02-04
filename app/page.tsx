18a5cc611d36a6bb9c1770e5ab210c09c4bc19a1
김우빈 <kwb020312@naver.com>
2024-02-04 11:53:46 +0900

    Feat: NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 const page = () => {
   return (
     <div>
+      <Navbar />
       <Live />
     </div>
   );
