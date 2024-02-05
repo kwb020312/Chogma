0602d47cb9af0ab20a49b9af4762f7d17ac25d1d
김우빈 <kwb020312@naver.com>
2024-02-05 22:59:44 +0900

    Feat: 도형 생성

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,31 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   handleCanvasMouseDown,
   handleResize,
   initializeFabric,
 } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 const Page = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas>(null);
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object>(null);
-  const selectedShapeRef = useRef<string | null>();
+  const selectedShapeRef = useRef<string | null>("rectangle");
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+    selectedShapeRef.current = elem?.value as string;
+  };
 
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef });
@@ -38,7 +50,10 @@ const Page = () => {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
       <section className="flex h-full flex-row">
         <LeftSidebar />
         <Live canvasRef={canvasRef} />
