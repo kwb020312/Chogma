4ecd82c1310b0db672bb4c384c4bcbf4ae56e66d
김우빈 <kwb020312@naver.com>
2024-02-08 21:37:10 +0900

    Feat: Canvas 우측 요소 수정 탭

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,13 @@ import {
   handleCanvasMouseDown,
   handleCanvasMouseUp,
   handleCanvasObjectModified,
+  handleCanvasSelectionCreated,
   handleCanvaseMouseMove,
   handleResize,
   initializeFabric,
   renderCanvas,
 } from "@/lib/canvas";
-import { ActiveElement } from "@/types/type";
+import { ActiveElement, Attributes } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
@@ -33,8 +34,19 @@ const Page = () => {
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
   const imageInputRef = useRef<HTMLInputElement>(null);
+  const isEditingRef = useRef(false);
 
   const canvasObjects = useStorage((root) => root.canvasObjects);
+
+  const [elementAttributes, setElementAttributes] = useState<Attributes>({
+    width: "",
+    height: "",
+    fontSize: "",
+    fontFamily: "",
+    fontWeight: "",
+    fill: "#aabbcc",
+    stroke: "#aabbcc",
+  });
 
   const syncShapeInStorage = useMutation(({ storage }, object) => {
     if (!object) return;
@@ -85,8 +97,9 @@ const Page = () => {
       case "delete":
         handleDelete(fabricRef.current as any, deleteShapeFromStorage);
         setActiveElement(defaultNavElement);
+        break;
       case "image":
-        imageInputRef?.current.click();
+        if (imageInputRef.current) imageInputRef.current.click();
         isDrawing.current = false;
 
         if (fabricRef.current) {
@@ -142,6 +155,14 @@ const Page = () => {
       });
     });
 
+    canvas.on("selection:created", (options: any) => {
+      handleCanvasSelectionCreated({
+        options,
+        isEditingRef,
+        setElementAttributes,
+      });
+    });
+
     window.addEventListener("resize", () => {
       handleResize({ canvas: fabricRef.current });
     });
@@ -186,7 +207,14 @@ const Page = () => {
       <section className="flex h-full flex-row">
         <LeftSidebar allShapes={Array.from(canvasObjects)} />
         <Live canvasRef={canvasRef} />
-        <RightSidebar />
+        <RightSidebar
+          elementAttributes={elementAttributes}
+          setElementAttributes={setElementAttributes}
+          fabricRef={fabricRef}
+          activeObjectRef={activeObjectRef}
+          isEditingRef={isEditingRef}
+          syncShapeInStorage={syncShapeInStorage}
+        />
       </section>
     </main>
   );
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,27 @@
-const RightSidebar = () => {
+import { RightSidebarProps } from "@/types/type";
+import Color from "./settings/Color";
+import Dimensions from "./settings/Dimensions";
+import Export from "./settings/Export";
+
+const RightSidebar = ({
+  elementAttributes,
+  setElementAttributes,
+  fabricRef,
+  activeObjectRef,
+  isEditingRef,
+  syncShapeInStorage,
+}: RightSidebarProps) => {
   return (
-    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
-      <h3 className="px-5 pt-4 text-xs">DESIGN</h3>
+    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 sticky right-0 h-full max-sm:hidden select-none">
+      <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
+      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+        원하는대로 캔버스를 꾸며보세요
+      </span>
+      <Dimensions />
+      <Text />
+      <Color />
+      <Color />
+      <Export />
     </section>
   );
 };
